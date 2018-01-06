70ae5daf8339c40d9a27940f2e52019029b08a03
Primuk <34391753+Primuk@users.noreply.github.com>
2018-01-06 13:03:14 +0530

    Wrote fibonacci in C language

--- fibonacci.c
+++ fibonacci.c
@@ -1,4 +1,20 @@
-#include <stdio.h>
-int main()
-print f
-return
+#include<stdio.h>
+main()
+{
+int i,n,a[10];
+printf("enter the number of terms");
+scanf("%d",&n);
+//printf("enter the elements");
+
+a[0]=0;
+a[1]=1;
+for(i=2;i<n;i++)
+{
+a[i]=a[i-1]+a[i-2];
+}
+printf("the fibonacci series is\t");
+for(i=0;i<n;i++)
+{
+printf("%d\n",a[i]);
+}
+}
